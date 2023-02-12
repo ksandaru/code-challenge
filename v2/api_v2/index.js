db9fdfb344b69b32e0b8e833618217cddb9ce891
kanishka_sb <kanishka.b@globalwavenet.com>
2023-02-12 14:06:15 +0530

    api index.js changes added

---
+++ v2/api_v2/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const  mongoose  = require("mongoose");
+const app = express();
+const users = require('./Routes/Users');
+const comments = require('./Routes/Comments');
+const connectDB = require('./config/database')
+
+
+// Connect Database
+connectDB();
+
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Enable CORS for all the requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("API started for v1");
+});
+
+//defined routes
+app.use('/users', users );
+app.use('/comments', comments );
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
