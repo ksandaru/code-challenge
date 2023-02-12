42210f58b2c7812f12d6639dff2689038504f73c
kanishka_sb <kanishka.b@globalwavenet.com>
2023-02-12 12:54:18 +0530

    CORS enabled

--- mvp/api_v1/index.js
+++ mvp/api_v1/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const  mongoose  = require("mongoose");
 const app = express();
-
-const connectDB = require('./Config/database')
+const users = require('./Routes/Users');
+const comments = require('./Routes/Comments');
+const connectDB = require('./config/database')
 
 
 // Connect Database
@@ -12,11 +13,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-
-app.get("/", (req, res) => {
-  res.send("API started v1");
+// Enable CORS for all the requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
+app.get("/", (req, res) => {
+  res.send("API started for v1");
+});
+
+//defined routes
+app.use('/users', users );
+app.use('/comments', comments );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
